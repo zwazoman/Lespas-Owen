bafb2220dcd80ca053dfa0e014e24db4a353d1eb
Asae <63079566+AsaeNeopie@users.noreply.github.com>
2024-03-15 02:03:36 +0100

    Scene menu
    (elle est magnifique)

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void GoPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Leave()
+    {
+        Application.Quit();
+        Debug.Log("Quitté");
+    }
+}
