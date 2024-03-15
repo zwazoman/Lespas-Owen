aa6cf4e9cfaa064c8a1a940003703ab5552b0509
zwazoman <gloupik.lemaire@gmail.com>
2024-03-15 15:34:28 +0100

    u

--- Assets/Scripts/Bullet/BulletDamagesThrough.cs
+++ Assets/Scripts/Bullet/BulletDamagesThrough.cs
@@ -14,6 +14,6 @@ public class BulletDamagesThrough : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.gameObject.SendMessage("ApplyDamage",damages) ;
-        //AudioManager.Instance.PlayBulletHit();
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.bulletHitSound);
     }
 }
